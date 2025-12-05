ac087f5c325b9c099ff73c26b23b414e1baf023d
Becks723 <1991737469@qq.com>
2025-12-05 18:15:40 +0800

    remove client call, leaving only api.

--- main.go
+++ main.go
@@ -1,38 +1,5 @@
 package pubpkg
 
-import (
-	"fmt"
-)
-
-func main() {
-	var x, y int
-	fmt.Printf("Enter number1: ")
-	fmt.Scan(&x)
-	fmt.Printf("Enter number2: ")
-	fmt.Scan(&y)
-
-	var op rune
-	fmt.Printf("Enter an operation (+-*/): ")
-	fmt.Scanf("%c", &op)
-
-	var result int
-	switch op {
-	case '+':
-		result = Add(x, y)
-	case '-':
-		result = Minus(x, y)
-	case '*':
-		result = Multiply(x, y)
-	case '/':
-		result = Divide(x, y)
-	default:
-		fmt.Printf("Unknown operation: %c", op)
-	}
-
-	fmt.Printf("Result is %d!", result)
-	fmt.Println()
-}
-
 func Add(x, y int) int {
 	return x + y
 }
